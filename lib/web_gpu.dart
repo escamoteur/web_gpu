dd92cd40a5f3a1894e27632f161062f37c697029
J-P Nurmi <jpnurmi@gmail.com>
2021-01-02 23:00:37 +0100

    Add WebGpu.ensureInitialized() for initializing the Dawn proc table

--- lib/web_gpu.dart
+++ lib/web_gpu.dart
@@ -1,14 +1,18 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class WebGpu {
-  static const MethodChannel _channel =
-      const MethodChannel('web_gpu');
+  static const MethodChannel _channel = const MethodChannel('web_gpu');
+  static bool _initialized = false;
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<void> ensureInitialized() async {
+    if (_initialized) return;
+    _initialized = await _channel.invokeMethod('init');
   }
 }
