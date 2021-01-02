84c95a30ef26d1ca2b302c789d7524b963c89dd1
J-P Nurmi <jpnurmi@gmail.com>
2021-01-02 23:50:33 +0100

    Add integration test for bindings

---
+++ example/integration_test/bindings_test.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi' as ffi;
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ffi/ffi.dart' as ffi;
+import 'package:integration_test/integration_test.dart';
+import 'package:web_gpu/src/dylib.dart';
+import 'package:web_gpu/web_gpu.dart';
+
+void main() => run(_testMain);
+
+void _testMain() async {
+  await WebGpu.ensureInitialized();
+
+  test('wgpuCreateInstance', () async {
+    final instance = dylib.wgpuCreateInstance(ffi.nullptr);
+    expect(instance, isNotNull);
+    expect(instance.address, isNonZero);
+    ffi.free(instance);
+  });
+}
